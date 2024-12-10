174278302666e34320b97e30e959933f4c00a78a
yichen_0403 <104032good@gmail.com>
2024-12-10 13:29:14 +0900

    改進速度

--- main.py
+++ main.py
@@ -7,6 +7,12 @@ import sub_process
 import os
 import logging
 from dotenv import load_dotenv
+import datetime
+from datetime import datetime
+import time
+
+
+
 load_dotenv()
 
 
@@ -62,15 +68,26 @@ async def text_autocompletion(interaction: discord.Interaction, current: str):
 
 @bot.tree.command(name="mygo", description="尋找MyGO台詞")
 @app_commands.autocomplete(text=text_autocompletion)
+@app_commands.describe(text="需要尋找的台詞")
+@app_commands.describe(second="延後秒數(可小數點)")
 async def mygo(interaction: discord.Interaction, text: str, second: float= 0.0):
     result = text_process(text)
+    start_time = datetime.now()
     await interaction.response.defer()
     episode = result[0]['episode']
     frame_start = result[0]['frame_start']
-    frame_end = result[0]['frame_end']
     image = sub_process.extract_frame(episode=episode, frame_number=frame_start, back_seconds=second)
-    await interaction.followup.send(file=discord.File(fp=image))
-    os.remove(image)
+    try:
+        await interaction.followup.send(file=discord.File(fp=image))
+        os.remove(image)
+        end_time = datetime.now()
+        timestamp = end_time.strftime("%Y-%m-%d %H:%M:%S")
+        run_time = end_time - start_time
+        total_seconds = run_time.total_seconds()
+        print(f"{timestamp}-->伺服器ID: {interaction.guild_id} 台詞: {text} 耗時: {total_seconds:.3f} 秒")
+    except Exception as e:
+        await interaction.followup.send("發生錯誤")
+        print(f"{timestamp}--> 伺服器ID: {interaction.guild_id} 台詞: {text} 錯誤: {e}")
     
 
 
--- sub_process.py
+++ sub_process.py
@@ -4,15 +4,16 @@ def extract_frame(episode, frame_number, back_seconds):
     # 计算时间戳
     episode = 'src/' + str(episode) + '.mp4'
     back_frames = round(back_seconds * 23.98)
-    frame_number = frame_number + back_frames
+    frame_number = frame_number + back_frames + 15
+    timestamp = frame_number / 23.98  # 时间戳（秒）
     # 使用 FFmpeg 提取特定帧
     output_image = 'output/' + str(frame_number) + '.png'
     ffmpeg_command = [
         "ffmpeg", 
+        "-ss", f"{timestamp:.6f}", 
         "-i", episode,  # 输入视频文件
-        "-vf", f"select=eq(n\\,{frame_number})",  # 精确选择帧
-        "-vsync", "vfr",  # 防止不必要的帧同步
-        "-vframes", "1",  # 仅提取一帧
+        "-frames:v", "1",
+        "-q:v", "2",                 # 高质量截图
         '-loglevel', 'error',
         output_image  # 输出的图像文件
     ]
